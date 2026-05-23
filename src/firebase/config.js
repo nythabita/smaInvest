cea0d8387dc4bb0914c77d401f907b5173dac52f
Naya <thabitakanaya98@gmail.com>
2026-05-23 18:41:38 +0700

    setup firebase config and router structure

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDkZpn0vsRvE7_w4Tr5LVlk4qdhuG1LJjk",
+    authDomain: "virtualcloset-f863f.firebaseapp.com",
+    projectId: "virtualcloset-f863f",
+    storageBucket: "virtualcloset-f863f.firebasestorage.app",
+    messagingSenderId: "425678904808",
+    appId: "1:425678904808:web:80113480683c305ce6585d",
+    measurementId: "G-N318X61ECS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
